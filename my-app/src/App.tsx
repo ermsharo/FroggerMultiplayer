339d0077a16304176d0d6fd62cdd50bce1173a74
emilio <ermsharo@gmail.com>
2022-11-01 04:00:42 -0300

    update changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Ground from './Components/RenderGround/Ground';
+import styled from 'styled-components';
+
+const DisplayBoard = styled.div`
+background-image:url("https://img.itch.zone/aW1hZ2UvNjQ5NDc4LzM0ODI3MTYuanBn/original/yqHXYK.jpg");
+`;
 
 function App() {
   return (
-    <div className="App">
+    <DisplayBoard>
 <Ground/>
-    </div>
+    </DisplayBoard>
   );
 }
 
---
+++ my-app/src/Components/RenderPlayers/Player_03.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import Tile from '../RenderTile/Tile';
+const DisplayGrid = styled.div`
+
+display: grid;
+grid-template-columns: repeat(22, 1fr);
+width: 60%;
+margin:auto;
+
+`;
+
+const DisplayTile = styled.div`
+width: 100%;
+aspect-ratio: 1 / 1;
+border: 1px solid red;
+`;
+
+
+
+const matrixDimensionWidth = 20;
+const matrixDimensionHeight = matrixDimensionWidth;
+
+
+let fillObj = (typeOfTile : any, tyle: any) => {
+  return tyle;
+};
+
+let fillLine = (line: any) => {
+  let ObjectLines = [];
+
+  for (let j = 0; j < line.length; j++) {
+    ObjectLines.push(fillObj("back", line[j]));
+  }
+  return ObjectLines;
+};
+
+let fillMatrix = (MapLines: any) => {
+  let i = 0;
+  let objMatrix = [];
+
+  while (i < MapLines.length) {
+    //console.log("->", MapLines[i]);
+    objMatrix.push(fillLine(MapLines[i]));
+    i++;
+  }
+  return objMatrix;
+};
+
+
+let createMatrix = (matrixDimension:number) =>{
+  return Array(matrixDimension).fill(null).map(() => Array(matrixDimension).fill(0));
+}
+
+let matrix : any = createMatrix(matrixDimensionWidth);
+matrix[1][0] = true;
+
+export default  function Ground() {
+  return (
+    <DisplayGrid>
+ {fillMatrix(MapLines).map((item: any) => (
+            item.map((tile: any) => (
+           <Tile  tile = {tile}/>
+                ))
+              ))}
+    </DisplayGrid>
+  );
+}
+
+
